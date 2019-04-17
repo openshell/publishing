198e327364a0bcc83c2a3d6d9effe032061dca98
openshell <C_Chasen@yeah.net>
2019-04-17 11:44:38 +0800

    引入热部署； 整合Thymeleaf+BootStrap+jQuery； 新建测试控制器，学习新的java模板

---
+++ src/main/java/com/cqz/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.cqz.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * @author openshell
+ * @date 2019/4/17
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("home")
+    public String goHome(Map<String ,Object> paraMap){
+        paraMap.put("name","openshell");
+        paraMap.put("age",24);
+        return "home";
+    }
+    @RequestMapping("login")
+    public String goLogin(){
+        return "login";
+    }
+}
